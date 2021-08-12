d0c2c5062fe1b9cb944c64df070b31875b9ed418
Michael Rider <mikepdxrider@gmail.com>
2021-08-11 18:00:54 -0700

    Updated html, css, and js files.

--- app.js
+++ app.js
@@ -6,3 +6,27 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+const catJpg = document.getElementById('catJpg');
+const catWav = document.getElementById('catWav');
+const dogJpg = document.getElementById('dogJpg');
+const dogWav = document.getElementById('dogWav');
+const horseJpg = document.getElementById('horseJpg');
+const horseWav = document.getElementById('horseWav');
+const header = document.getElementById('header');
+
+catJpg.addEventListener('click', () => {
+  catWav.play();
+  header.innerText = 'Tigger the tiger said "MEOW!"';
+});
+
+dogJpg.addEventListener('click', () => {
+  dogWav.play();
+  header.innerText = 'Astro the pupper said "RUFF!"';
+});
+
+horseJpg.addEventListener('click', () => {
+  horseWav.play();
+  header.innerText = 'Khan the horsey said "NAYYYY!"';
+});
+
